70299b78e9bb95576ed51aca58bab42220951d1f
Steven Lu <tacticalazn@gmail.com>
2013-03-06 00:49:36 -0500

    GPS coordinates to zip codes.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Zippy
+
+Zippy is an easy to use node.js library which is optimized to find your a US based zip code based on the GPS coordinates you give it.
+
+## Instructions
+
+Like any node.js application.
+
+    npm install
+    node app.js
---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var locations = JSON.parse(fs.readFileSync('locations.json', 'ascii'));
+
+app.get('/', function (req, res)
+{
+   res.send('To use, send in a request with coordinates. For example, /40.672/-74.520');
+});
+
+app.get('/:lat/:lng', function (req, res)
+{
+   var lat = req.params.lat;
+   var lng = req.params.lng;
+
+   var min_distance = 9999999999; // simulate infinity
+   var min_location = {};
+
+   for (var i = 0; i < locations.length; i++)
+   {
+      var distance = Math.sqrt(
+         Math.pow(lat - locations[i].latitude, 2) + Math.pow(lng - locations[i].longitude, 2)
+      );
+
+      if (distance < min_distance)
+      {
+         min_location = locations[i];
+         min_distance = distance;
+      }
+   }
+
+   res.send(min_location);
+});
+
+app.listen(4000);
+console.log('http://localhost:4000');
