00e33a5247d4eebb21964245ea845325b69feeaa
Steven Lu <tacticalazn@gmail.com>
2013-03-06 11:11:17 -0500

    Fixing up formula.

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ var app = express();
 
 var locations = JSON.parse(fs.readFileSync('locations.json', 'ascii'));
 
+var R = 6371;
+var haversine = function(lat1, lon1, lat2, lon2)
+{
+   var dLat = (lat2-lat1) * (Math.PI / 180);
+   var dLon = (lon2-lon1) * (Math.PI / 180);
+   var lat1 = lat1 * (Math.PI / 180);
+   var lat2 = lat2 * (Math.PI / 180);
+
+   var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+           Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+   var d = R * c;
+
+   return d;
+}
+
 exports.zip_lookup = function(zip)
 {
    for (var i = 0; i < locations.length; i++)
@@ -17,17 +33,17 @@ exports.gps_lookup = function(lat, lng)
 {
    var min_distance = 9999999999; // simulate infinity
    var min_location = {};
-
+   
    for (var i = 0; i < locations.length; i++)
    {
-      var distance = Math.sqrt(
-         Math.pow(lat - locations[i].latitude, 2) + Math.pow(lng - locations[i].longitude, 2)
-      );
+      var distance = haversine(lat, lng, locations[i].latitude, locations[i].longitude); 
 
       if (distance < min_distance)
       {
          min_location = locations[i];
          min_distance = distance;
+
+         console.log(min_location);
       }
    }
 
