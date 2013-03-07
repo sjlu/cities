08833bafddf91bcde29838fcaff57240a323e4b6
Steven Lu <tacticalazn@gmail.com>
2013-03-07 18:43:53 -0500

    Error handling in server.js

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.get('/zip/:zip', function (req, res)
 {
    var zip = req.params.zip;
 
-   res.send(cities.zip_lookup(zip));
+   var response = cities.zip_lookup(zip);
+   if (!response)
+      return res.send({error:'Zip code is not valid.'});
+
+   return res.send(response);
 });
 
 app.get('/gps/:lat/:lng', function (req, res)
@@ -21,7 +25,7 @@ app.get('/gps/:lat/:lng', function (req, res)
    var lat = req.params.lat;
    var lng = req.params.lng;
 
-   res.send(cities.gps_lookup(lat, lng));
+   return res.send(cities.gps_lookup(lat, lng));
 });
 
 app.listen(4000);
